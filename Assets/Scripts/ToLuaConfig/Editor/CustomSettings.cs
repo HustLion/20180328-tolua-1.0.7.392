b1a1ff45886edf5f3052a009a54199c6dadd2e48
HustLion <hustlionm@qq.com>
2018-03-28 11:12:38 +0800

    moved ToLua

--- Assets/Scripts/ToLuaConfig/Editor/CustomSettings.cs
+++ Assets/Scripts/ToLuaConfig/Editor/CustomSettings.cs
@@ -10,7 +10,7 @@ using System.Reflection;
 public static class CustomSettings
 {
     public static string saveDir = Application.dataPath + "/Scripts/Generated/ToLua/";    
-    public static string toluaBaseType = Application.dataPath + "/ToLua/BaseType/";    
+    public static string toluaBaseType = Application.dataPath + "/ThirdParty/ToLua/BaseType/";    
 
     //导出时强制做为静态类的类型(注意customTypeList 还要添加这个类型才能导出)
     //unity 有些类作为sealed class, 其实完全等价于静态类
